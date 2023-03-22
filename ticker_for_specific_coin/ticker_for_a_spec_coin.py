bd070d74a4fba6cf236ea9d9c4a0ce6d56a4f4fa
jabiraziz <jabiraziz430@gmail.com>
2023-03-22 14:32:32 +0500

    added change field

--- ticker_for_specific_coin/ticker_for_a_spec_coin.py
+++ ticker_for_specific_coin/ticker_for_a_spec_coin.py
@@ -8,18 +8,20 @@ from helper import call_api
 api_key = os.environ.get('API_KEY')
 
 def ticker_for_a_spec_coin():
-    symbols = CoinProfile.objects.values_list('symbol', flat=True)
+    symbols = CoinProfile.objects.values_list('symbol', flat=True)[3050:]
     for symbol in symbols:
         url = f"https://financialmodelingprep.com/api/v3/quote/{symbol}?apikey={api_key}"
         response_data = call_api(url)
         if response_data:
             for fmp_coin_response in response_data:
                 symbol = fmp_coin_response.get('symbol')
+                print(symbol)
                 coin_profile = CoinProfile.objects.get(symbol=symbol)
                 #this part of data is coming from Fmp api
                 defaults={
                     'price': fmp_coin_response.get("price"),
                     'change_percentage': fmp_coin_response.get("changesPercentage"),
+                    'change': fmp_coin_response.get("change"),
                     'market_cap': fmp_coin_response.get("marketCap"),
                     'year_high': fmp_coin_response.get("yearHigh"),
                     'year_low': fmp_coin_response.get("yearLow")
