a19dcd1abb5f61e0942c23d71021511d1f91f239
jabiraziz <jabiraziz430@gmail.com>
2023-03-22 14:35:03 +0500

    added new app , configure urls

--- coin_data/urls.py
+++ coin_data/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('api/',include('coin_links.urls')),
     path('api/',include('coin_today_ohlc.urls')),
     path('api/',include('performance.urls')),
-    path('api/',include('ticker_for_specific_coin.urls'))
+    path('api/',include('ticker_for_specific_coin.urls')),
+    path('api/',include('crypto_historical_data.urls'))
 ]
