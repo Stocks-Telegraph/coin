10f1330f738fc96e98356ac215822e3c555d4e79
jabiraziz <jabiraziz430@gmail.com>
2023-03-26 20:36:17 +0500

    added all social and explorer links to db

--- coin_links/api_scripts/coinpaprika_social_links.py
+++ coin_links/api_scripts/coinpaprika_social_links.py
@@ -3,7 +3,7 @@ from coin_profile.models import CoinProfile
 from helper import call_api
 
 def coinpaprika_social_links():
-    ids = CoinProfile.objects.values_list('coin_id', flat=True)[1350:1420]
+    ids = CoinProfile.objects.values_list('coin_id', flat=True)[3300:]
     coins_data = []
     for id in ids:
         url = f"https://api.coinpaprika.com/v1/coins/{id}"
