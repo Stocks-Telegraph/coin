52d1a88f521b62455fee8992f974cbe7ca5215a5
jabiraziz <jabiraziz430@gmail.com>
2023-03-26 20:34:00 +0500

    api call (sent request)

--- helper.py
+++ helper.py
@@ -1,13 +1,7 @@
 import requests
-import time
-import random
-
 
 def call_api(URL, return_value=None):
-    print(URL)
     response = requests.get(URL)
-    print(response)
-    time.sleep(random.randrange(8,11))
     if response.status_code == 200:
         return response.json()
     return return_value
